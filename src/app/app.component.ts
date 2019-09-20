8761f403b5c1a271f6f64db2fbb8c9d46731e616
MichaelHaraux <michaelifa57@gmail.com>
2019-09-20 14:41:43 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+
+
+  postlistcomponents = [
+    {
+      title: 'Mon premier post',
+      content: 'Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia,',
+      loveIts: 0,
+      created_at: Date
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia,',
+      loveIts: 0,
+      created_at: Date
+    },
+    {
+      title: 'Mon troisième post',
+      content: 'Cum haec taliaque sollicitas eius aures everberarent expositas semper eius modi rumoribus et patentes, varia animo tum miscente consilia,',
+      loveIts: 0,
+      created_at: Date
+    }
+  ];
+
 }
---
+++ src/app/post-list-component/post-list-component.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-component',
+  templateUrl: './post-list-component.component.html',
+  styleUrls: ['./post-list-component.component.css']
+})
+export class PostListComponentComponent implements OnInit {
+  @Input() PostListComponentTitle: string ;
+  @Input() PostListComponentContent: string;
+  @Input() PostListComponentLoveIts: number; 
+  @Input() PostListComppnentCreated_at: Date;
+   
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+ 
+}
